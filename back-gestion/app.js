0e15187ff719d8816ab427d739b894253514587f
LouiseDupuis <louise.dupuis97@gmail.com>
2018-10-27 02:52:34 +0200

    Mise à jour des options de credentials

--- back-gestion/app.js
+++ back-gestion/app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:4200',
   credentials: true };
 
 app.use(cors(corsOptions));
